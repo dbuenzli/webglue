0f6a09ff01f6f7201c6c2e0822164ff0b7ec80d0
Daniel Bünzli <daniel.buenzli@erratique.ch>
2025-03-10 10:29:36 +0100

    Remove some watermarks.

--- src/api/seio.ml
+++ src/api/seio.ml
@@ -1,7 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2011 Daniel C. Bünzli. All rights reserved.
    SPDX-License-Identifier: ISC
-   %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
 let u_bsl  = 0x005C (* backslash *)
--- src/tool/chelp.mli
+++ src/tool/chelp.mli
@@ -1,7 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2011 Daniel C. Bünzli. All rights reserved.
    SPDX-License-Identifier: ISC
-   %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
 (** The [help] command. *)
